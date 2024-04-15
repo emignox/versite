9c510d67521a93fbdf3810daf3cad6c44556be1b
emanuele <emanuele971@icloud.com>
2024-04-15 13:18:12 +0200

    try to deploy on vercel and connection url

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,15 +1,15 @@
-const express = require("express"); // Import express
-const userRoutes = require("../routes/userRoutes"); // Import userRoutes
-require("dotenv").config(); // Import dotenv
-const app = express(); // Initialize the app with express
-const connect = require("../db"); // Import the connect function from db.js
+const express = require("express");
+const userRoutes = require("../routes/userRoutes");
+require("dotenv").config();
+const app = express();
+const connect = require("../db");
 const postRoutes = require("../routes/postRoutes");
-const cookiePaerser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const { checkUser } = require("../middleware/auth.middleware");
 const { requireAuth } = require("../middleware/auth.middleware");
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 
-app.use(express.json()); // Use express.json middleware
+app.use(express.json());
 
 const corsOptions = {
   origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_2],
@@ -19,31 +19,25 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
 };
+
 app.use(cors(corsOptions));
-console.log(
-  `CORS is configured to accept requests from: ${process.env.CLIENT_URL}`
-);
+app.use(cookieParser());
+
+app.get("*", checkUser);
+
 app.get("/ciao", (req, res) => {
   res.status(200).send("ciao");
 });
 
-//routes
-app.use("/user", userRoutes); // Use userRoutes
-app.use(cookiePaerser());
-// authorisation cors
-
+app.use("/user", userRoutes);
 app.use("/post", postRoutes);
 
-// Serve static files from the "uploads" directory
-// ... il resto del tuo codice server qui ...
-//jwt
-app.get("*", checkUser);
 app.get("/jwtid", requireAuth, (req, res) => {
   res.status(200).send(res.locals.user._id);
 });
+
 const port = process.env.PORT || 3000;
 
-//server.js
 app.listen(port, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
